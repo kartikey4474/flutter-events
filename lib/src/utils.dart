c83f9606e839cb308a1a27b817741c1a542c973e
kartikey4474 <73388744+kartikey4474@users.noreply.github.com>
2024-03-31 21:00:17 +0530

    first commit

---
+++ lib/src/utils.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static Color darkPink = const Color.fromARGB(255, 0, 17, 51);
+  static Color darkColor = const Color.fromARGB(255, 0, 25, 76);
+  static Color pinkColor = const Color.fromARGB(255, 0, 51, 102);
+  static Color orangePink = const Color.fromARGB(255, 0, 68, 136);
+  static Color whitishPink = const Color.fromARGB(255, 16, 78, 139);
+}
